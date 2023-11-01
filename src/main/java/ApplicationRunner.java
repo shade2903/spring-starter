f0ebafb5401a89fb360c84fa3d6735035c1cb095
Vladimir Haiduk <nightshade2903@gmail.com>
2023-11-01 22:49:01 +0300

    initial commit

--- src/main/java/ApplicationRunner.java
+++ src/main/java/ApplicationRunner.java
@@ -1,4 +1,5 @@
 import com.dmdev.spring.database.pool.ConnectionPool;
+import com.dmdev.spring.database.repository.CompanyRepository;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
@@ -7,5 +8,7 @@ public class ApplicationRunner {
 //      clazz -> String -> Map<String, Object>
         var connectionPool = context.getBean("p1", ConnectionPool.class);
         System.out.println(connectionPool);
+        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        System.out.println(companyRepository);
     }
 }
